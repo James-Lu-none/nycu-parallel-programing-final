6bd83a5f13dc3d1426f1365284ccaf33634bf6a8
James-Lu-none <jamesabcde277@gmail.com>
2025-10-08 01:55:26 +0800

    add config.hpp

---
+++ final/include/config.hpp
@@ -0,0 +1,21 @@
+#pragma once
+#include <cstdint>
+#include "planet.hpp"
+#include "accelerations.hpp"
+#include "TracyOpenCL.hpp"
+
+#define WIDTH 1200
+#define HEIGHT 800
+
+#define NUM_BODIES 3
+#define TRAIL_BUF 100
+#define MIN_DIST 1.5
+
+#define G 10000.0
+#define EPSILON 1e-6
+
+#define COL_BLACK 0x00000000
+
+#define NUM_THREADS 4
+
+#define view_z 100.0
