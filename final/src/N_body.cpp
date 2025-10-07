344f9d242f33a4cbb6652f6e5321cc1d848c8982
James-Lu-none <jamesabcde277@gmail.com>
2025-10-08 01:56:12 +0800

    move N_body.cpp

--- final/src/N_body.cpp
+++ final/src/N_body.cpp
@@ -5,34 +5,9 @@
 #include <ctime>
 #include <cstdlib>
 
-#include "tracy/public/TracyClient.cpp"
-#include "tracy/public/tracy/TracyOpenCL.hpp"
+#include "config.hpp"
 
 using namespace std;
-
-#define WIDTH   1200
-#define HEIGHT  800
-
-#define NUM_BODIES 3
-#define TRAIL_BUF  100
-#define MIN_DIST   1.5
-
-#define G        10000.0
-#define EPSILON  1e-6
-
-#define COL_BLACK      0x00000000
-    uint32_t COLORS[] = {0x00ff0000, 0x0000ff00, 0x000000ff, 0x00ffff00, 0x00ff00ff, 0x0000ffff};
-
-#define NUM_THREADS 4
-
-#define view_z 100.0
-typedef struct {
-    double x, y, z;
-    double vx, vy, vz;
-    double ax, ay, az;
-    double mass;
-    double r;
-} Planet;
 
 typedef struct {
     int x[TRAIL_BUF];
@@ -120,197 +95,13 @@ static void trail_draw(SDL_Surface *surf, const Trail *t, Uint32 col)
     }
 }
 
-typedef struct
-{
-    const Planet *b;
-    int t_id;
-    int t_N;
-    double *t_ax;
-    double *t_ay;
-    double *t_az;
-} AccelerationArgsV1;
-
-static void *accelerations_thread_v1(void *arg)
-{
-    AccelerationArgsV1 *A = (AccelerationArgsV1 *)arg;
-    const Planet *b = A->b;
-    int t_id = A->t_id;
-    int t_N = A->t_N;
-
-    double *ax = A->t_ax;
-    double *ay = A->t_ay;
-    double *az = A->t_az;
-
-    // add t_N-1 to NUM_BODIES before divide by t_N to ensure all acceleration pairs (i,j) are covered
-    int chunk = (NUM_BODIES + t_N - 1) / t_N;
-    int i_start = t_id * chunk;
-    int i_end = (i_start + chunk < NUM_BODIES) ? (i_start + chunk) : NUM_BODIES;
-    int count = 0;
-
-    for (int i = i_start; i < i_end; ++i)
-    {
-        for (int j = i + 1; j < NUM_BODIES; ++j)
-        {
-            double dx = b[j].x - b[i].x;
-            double dy = b[j].y - b[i].y;
-            double dz = b[j].z - b[i].z;
-            double dist2 = dx * dx + dy * dy + dz * dz + EPSILON;
-            double dist = sqrt(dist2);
-
-            double F = (G * b[i].mass * b[j].mass) / dist2;
-            double fx = F * dx / dist;
-            double fy = F * dy / dist;
-            double fz = F * dz / dist;
-
-            ax[i] += fx / b[i].mass;
-            ay[i] += fy / b[i].mass;
-            az[i] += fz / b[i].mass;
-            ax[j] -= fx / b[j].mass;
-            ay[j] -= fy / b[j].mass;
-            az[j] -= fz / b[j].mass;
-            count++;
-        }
-    }
-    printf("Thread %d: i_start=%d, i_end=%d, calc_count=%d\n", t_id, i_start, i_end, count);
-    return NULL;
-}
-
-static void *accelerations_thread_v2(void *arg)
-{
-    AccelerationArgsV1 *A = (AccelerationArgsV1 *)arg;
-    const Planet *b = A->b;
-    int t_id = A->t_id;
-    int t_N = A->t_N;
-
-    double *ax = A->t_ax;
-    double *ay = A->t_ay;
-    double *az = A->t_az;
-
-    // add t_N-1 to NUM_BODIES before divide by t_N to ensure all acceleration pairs (i,j) are covered
-    int chunk = (NUM_BODIES + t_N - 1) / t_N;
-    int i_start = t_id * chunk;
-    int i_end = (i_start + chunk < NUM_BODIES) ? (i_start + chunk) : NUM_BODIES;
-    int count = 0;
-
-    for (int i = i_start; i < i_end; ++i)
-    {
-        for (int j = 0; j < NUM_BODIES; ++j)
-        {
-            double dx = b[j].x - b[i].x;
-            double dy = b[j].y - b[i].y;
-            double dz = b[j].z - b[i].z;
-            double dist2 = dx * dx + dy * dy + dz * dz + EPSILON;
-            double dist = sqrt(dist2);
-
-            double F = (G * b[i].mass * b[j].mass) / dist2;
-            double fx = F * dx / dist;
-            double fy = F * dy / dist;
-            double fz = F * dz / dist;
-            ax[i] += fx / b[i].mass;
-            ay[i] += fy / b[i].mass;
-            az[i] += fz / b[i].mass;
-            count++;
-        }
-    }
-    printf("Thread %d: i_start=%d, i_end=%d, calc_count=%d\n", t_id, i_start, i_end, count);
-    return NULL;
-}
-
-static void accelerations_parallel(Planet b[])
-{
-    ZoneScopedN("accelerations_parallel");
-
-    int t_N = NUM_THREADS > NUM_BODIES ? NUM_BODIES : NUM_THREADS;
-
-    pthread_t *threads = (pthread_t *)malloc(sizeof(pthread_t) * t_N);
-    AccelerationArgsV1 *args = (AccelerationArgsV1 *)malloc(sizeof(AccelerationArgsV1) * t_N);
-
-    double **t_ax = (double **)malloc(sizeof(double *) * t_N);
-    double **t_ay = (double **)malloc(sizeof(double *) * t_N);
-    double **t_az = (double **)malloc(sizeof(double *) * t_N);
-    for (int t = 0; t < t_N; ++t)
-    {
-        t_ax[t] = (double *)calloc(NUM_BODIES, sizeof(double));
-        t_ay[t] = (double *)calloc(NUM_BODIES, sizeof(double));
-        t_az[t] = (double *)calloc(NUM_BODIES, sizeof(double));
-    }
-
-    for (int i = 0; i < NUM_BODIES; ++i) b[i].ax = b[i].ay = b[i].az = 0.0;
-
-    for (int t = 0; t < t_N; ++t)
-    {
-        args[t].b = b;
-        args[t].t_id = t;
-        args[t].t_N = t_N;
-        args[t].t_ax = t_ax[t];
-        args[t].t_ay = t_ay[t];
-        args[t].t_az = t_az[t];
-        pthread_create(&threads[t], NULL, accelerations_thread_v2, &args[t]);
-    }
-
-    for (int t = 0; t < t_N; ++t)
-    {
-        pthread_join(threads[t], NULL);
-    }
-
-    for (int t = 0; t < t_N; ++t)
-    {
-        for (int i = 0; i < NUM_BODIES; ++i)
-        {
-            b[i].ax += t_ax[t][i];
-            b[i].ay += t_ay[t][i];
-            b[i].az += t_az[t][i];
-        }
-    }
-    
-    for (int t = 0; t < t_N; ++t)
-    {
-        free(t_ax[t]);
-        free(t_ay[t]);
-        free(t_az[t]);
-    }
-    free(t_ax);
-    free(t_ay);
-    free(t_az);
-    free(threads);
-    free(args);
-}
-
-static void accelerations(const Planet b[], double ax[], double ay[], double az[])
-{
-    ZoneScopedN("accelerations");
-    for (int i = 0; i < NUM_BODIES; ++i) ax[i] = ay[i] = az[i] = 0.0;
-
-    for (int i = 0; i < NUM_BODIES; ++i) {
-        for (int j = i + 1; j < NUM_BODIES; ++j) {
-            double dx = b[j].x - b[i].x;
-            double dy = b[j].y - b[i].y;
-            double dz = b[j].z - b[i].z;
-            double dist2 = dx*dx + dy*dy + dz*dz + EPSILON;
-            double dist  = sqrt(dist2);
-
-            double F  = (G * b[i].mass * b[j].mass) / dist2;
-            double fx = F * dx / dist;
-            double fy = F * dy / dist;
-            double fz = F * dz / dist;
-
-            ax[i] +=  fx / b[i].mass;
-            ay[i] +=  fy / b[i].mass;
-            az[i] +=  fz / b[i].mass;
-            ax[j] -=  fx / b[j].mass;
-            ay[j] -=  fy / b[j].mass;
-            az[j] -=  fz / b[j].mass;
-        }
-    }
-}
-
 static void step_leapfrog(Planet b[], double dt)
 {
     static int first = 1;
 
     if (first) {
         ZoneScopedN("step_leapfrog_first");
-        accelerations_parallel(b);
+        accelerations(b);
         first = 0;
     }
 
@@ -323,7 +114,7 @@ static void step_leapfrog(Planet b[], double dt)
         b[i].z  +=      b[i].vz * dt;
     }
 
-    accelerations_parallel(b);
+    accelerations(b);
 
     for (int i = 0; i < NUM_BODIES; ++i) {
         ZoneScopedN("step_leapfrog");
@@ -388,6 +179,7 @@ int main(void)
         return 1;
     }
 
+    const unsigned long colors[] = {0x00ff0000, 0x0000ff00, 0x000000ff, 0x00ffff00, 0x00ff00ff, 0x0000ffff};
     Planet bodies[NUM_BODIES];
     const double S  = 140.0;
     const double VS = 140.0;
@@ -440,8 +232,8 @@ int main(void)
         SDL_FillRect(surf, NULL, COL_BLACK);
         
         for (int i = 0; i < NUM_BODIES; ++i) {
-            trail_draw(surf, &trails[i], COLORS[i % 6]);
-            fill_circle(surf, (int)bodies[i].x, (int)bodies[i].y, (int)bodies[i].z, (int) bodies[i].r, COLORS[i % 6]);
+            trail_draw(surf, &trails[i], colors[i % 6]);
+            fill_circle(surf, (int)bodies[i].x, (int)bodies[i].y, (int)bodies[i].z, (int) bodies[i].r, colors[i % 6]);
         }
 
         SDL_UpdateWindowSurface(win);
