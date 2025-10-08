bdc1445b5dd694d91ff2d8d5f8bd60d1b9ebef68
James-Lu-none <jamesabcde277@gmail.com>
2025-10-08 09:34:03 +0800

    fix: correct typo

--- final/src/accelerations/accelerations_pthread_pair.cpp
+++ final/src/accelerations/accelerations_pthread_pair.cpp
@@ -62,7 +62,7 @@ void accelerations(Planet b[])
     int t_N = NUM_THREADS > NUM_BODIES ? NUM_BODIES : NUM_THREADS;
 
     pthread_t *threads = (pthread_t *)malloc(sizeof(pthread_t) * t_N);
-    AccelerationArgsV1 *args = (AccelerationArgsV1 *)malloc(sizeof(AccelerationArgsV1) * t_N);
+    AccelerationArgs *args = (AccelerationArgs *)malloc(sizeof(AccelerationArgs) * t_N);
 
     double **t_ax = (double **)malloc(sizeof(double *) * t_N);
     double **t_ay = (double **)malloc(sizeof(double *) * t_N);
@@ -85,7 +85,7 @@ void accelerations(Planet b[])
         args[t].t_ax = t_ax[t];
         args[t].t_ay = t_ay[t];
         args[t].t_az = t_az[t];
-        pthread_create(&threads[t], NULL, accelerations_thread_v2, &args[t]);
+        pthread_create(&threads[t], NULL, accelerations_thread, &args[t]);
     }
 
     for (int t = 0; t < t_N; ++t)
