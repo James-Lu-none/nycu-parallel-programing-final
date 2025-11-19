3b577876589f6ca65b92c22d5bc36aa24e735cdc
James-Lu-none <jamesabcde277@gmail.com>
2025-11-19 17:23:25 +0800

    feat: add color and canvas structs for rendering

--- include/render.hpp
+++ include/render.hpp
@@ -2,6 +2,18 @@
 #include "planet.hpp"
 #include "config.hpp"
 
+struct color
+{
+    uint8_t r;
+    uint8_t g;
+    uint8_t b;
+};
+
+struct canvas
+{
+    color pixels[WIDTH * HEIGHT];
+};
+
 void recenter(Planet b[]);
 void fill_circle(SDL_Surface *surf, int cx, int cy, int cz, int rad, Uint32 col);
 void trail_push(Trail *t, int x, int y, int z);
