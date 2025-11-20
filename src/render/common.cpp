f4ea3bae27147430af94fe3b3cd21f646d7e2444
Timyu0527 <ywtcse@gmail.com>
2025-11-20 17:50:33 +0800

    fix: add common.cpp for common method

---
+++ src/render/common.cpp
@@ -0,0 +1,38 @@
+#include "render.hpp"
+#include "planet.hpp"
+#include "vec3.hpp"
+#include "ray.hpp"
+
+bool hit_trail(const Trail &t, const ray &r)
+{
+    int radius_squared = 4; // radius 2
+    for (int i = 0; i < t.size; ++i)
+    {
+        vec3 oc = t.pos[i] - r.origin();
+        float a = dot(r.direction(), r.direction());
+        float b = -2.0 * dot(r.direction(), oc);
+        float c = dot(oc, oc) - radius_squared;
+        float discriminant = b * b - 4 * a * c;
+        if (discriminant >= 0) {
+            return true;
+        }
+    }
+    return false;
+}
+
+float hit_planet(const Planet &p, const ray &r)
+{
+    vec3 oc = p.pos - r.origin();
+    float a = dot(r.direction(), r.direction());
+    float b = -2.0 * dot(r.direction(), oc);
+    float c = dot(oc, oc) - p.r * p.r;
+    float discriminant = b * b - 4 * a * c;
+    if (discriminant < 0)
+    {
+        return -1.0;
+    }
+    else
+    {
+        return (-b - sqrt(discriminant)) / (2.0 * a);
+    }
+}
--- src/render/render_pthread_simd.cpp
+++ src/render/render_pthread_simd.cpp
@@ -168,65 +168,6 @@ color get_ray_color_simd(const ray &r, const Planet* bodies, const Trail* trails
 }
 
 
-color get_ray_color(const ray &r, const Planet* bodies, const Trail* trails)
-{
-    for (int i = 0; i < NUM_BODIES; ++i)
-    {
-        float t = hit_planet(bodies[i], r);
-        if (t >= 0)
-        {
-            vec3 N = 128 * (unit_vector(r.at(t) - bodies[i].pos) + vec3(1, 1, 1));
-            // printf("N: (%f, %f, %f)\n", N.x(), N.y(), N.z());
-            return {
-                (uint8_t)std::min(N.x(), (float)255.0),
-                (uint8_t)std::min(N.y(), (float)255.0),
-                (uint8_t)std::min(N.z(), (float)255.0),
-                255
-            };
-            // return bodies[i].col;
-        }
-        // if (hit_trail(trails[i], r))
-        // {
-        //     return bodies[i].col;
-        // }
-    }
-    return {0, 0, 0, 255};
-}
-
-bool hit_trail(const Trail &t, const ray &r)
-{
-    int radius_squared = 4; // radius 2
-    for (int i = 0; i < t.size; ++i)
-    {
-        vec3 oc = t.pos[i] - r.origin();
-        float a = dot(r.direction(), r.direction());
-        float b = -2.0 * dot(r.direction(), oc);
-        float c = dot(oc, oc) - radius_squared;
-        float discriminant = b * b - 4 * a * c;
-        if (discriminant >= 0) {
-            return true;
-        }
-    }
-    return false;
-}
-
-float hit_planet(const Planet &p, const ray &r)
-{
-    vec3 oc = p.pos - r.origin();
-    float a = dot(r.direction(), r.direction());
-    float b = -2.0 * dot(r.direction(), oc);
-    float c = dot(oc, oc) - p.r * p.r;
-    float discriminant = b * b - 4 * a * c;
-    if (discriminant < 0)
-    {
-        return -1.0;
-    }
-    else
-    {
-        return (-b - sqrt(discriminant)) / (2.0 * a);
-    }
-}
-
 void *render_thread(void *args_void){
     ZoneScopedN("rendering thread");
     RenderTaskArgs *args = (RenderTaskArgs *)args_void;
