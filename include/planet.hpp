c81d4e8440077ef639e735c1c633bcd650e5f2fe
James-Lu-none <jamesabcde277@gmail.com>
2025-11-22 16:16:34 +0800

    add PlanetsSoA struct

--- include/planet.hpp
+++ include/planet.hpp
@@ -16,6 +16,32 @@ typedef struct
 
 typedef struct
 {
+    int count;
+
+    alignas(32) float *x;
+    alignas(32) float *y;
+    alignas(32) float *z;
+
+    alignas(32) float *vx;
+    alignas(32) float *vy;
+    alignas(32) float *vz;
+
+    alignas(32) float *ax;
+    alignas(32) float *ay;
+    alignas(32) float *az;
+
+    alignas(32) float *mass;
+    alignas(32) float *r;
+
+    // store color as 0–255 bytes
+    uint8_t *col_r;
+    uint8_t *col_g;
+    uint8_t *col_b;
+    uint8_t *col_a;
+} PlanetsSoA;
+
+typedef struct
+{
     point3 pos[TRAIL_BUF];
     int head;
     int size;
